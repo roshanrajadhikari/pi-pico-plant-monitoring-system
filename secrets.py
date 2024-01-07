fb956e82c7282a044fd2e283ec28c5e7fb9dc8d8
roshanrajadhikari <63466970+roshanrajadhikari@users.noreply.github.com>
2024-01-07 20:49:37 +1100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,9 @@
+#network details
+WIFI_SSID = "" #wifi name aka SSID
+WIFI_PASSWORD = "" #password for network
+
+#mqtt server details
+MQTT_HOST = "" #host ip address
+MQTT_USERNAME = "" #client username
+MQTT_PASSWORD = "" #client password
+
