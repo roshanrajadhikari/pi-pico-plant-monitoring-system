0f32b2f997bba507694a6615406095dd94f465df
roshanrajadhikari <roshanrazzadhikary@gmail.com>
2024-01-07 20:25:30 +1100

    added few comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from broker import Broker
 from sensor import Sensor
 import time
 
-sensor = Sensor(28,50000,23000) #sensor instance
+sensor = Sensor(28,50000,23000) #sensor instance params: ADC pin, value when no moisture, value when max moisture
 broker = Broker() # service broker instance
 topic = "dev" #topic to publish data in mqtt
 
--- sensor.py
+++ sensor.py
@@ -7,7 +7,7 @@ class Sensor:
         self.pin = pin 
         self.min = min
         self.max = max
-        self.sensor = ADC(pin)
+        self.sensor = ADC(pin) #setting sensor ADC pin
     
     # A handy function for mapping values
     def __map_range(self,x, in_min_val, in_max_val, out_min, out_max):
@@ -15,6 +15,6 @@ class Sensor:
         return int(max(min(mapped, out_max), out_min))
 
     def readData(self):
-        raw_data_reading = self.sensor.read_u16()
-        filtered_data = self.__map_range(raw_data_reading, self.min, self.max, 0, 100)
-        return filtered_data
+        raw_data_reading = self.sensor.read_u16() #reading the raw value from sensor
+        filtered_data = self.__map_range(raw_data_reading, self.min, self.max, 0, 100) #mapping value between 0 and 100
+        return filtered_data #return human readable percentage data
